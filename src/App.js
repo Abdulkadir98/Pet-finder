74e597bc2f7d1bb9eb36cea54c87305821edc512
Abdul Kadir <ak98.narutokun@gmail.com>
2019-04-17 20:19:23 +0530

    Add Search Context and make SearchBox a reusable component anywhere

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
 import React from "react";
 import { render } from "react-dom";
-import Results from "./Results";
 import { Router, Link } from "@reach/router";
+import pf from "petfinder-client";
+import { Provider } from "./SearchContext";
+import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
+const petfinder = pf();
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: "Seattle, WA",
+      animal: "",
+      breed: "",
+      breeds: [],
+      handleAnimalChange: this.handleAnimalChange,
+      handleBreedChange: this.handleBreedChange,
+      handleLocationChange: this.handleLocationChange,
+      getBreeds: this.getBreeds
+    };
+  }
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+
+  handleAnimalChange = event => {
+    this.setState(
+      {
+        animal: event.target.value,
+        breed: ""
+      },
+      this.getBreeds
+    );
+  };
+
+  handleBreedChange = event => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed.list({ animal: this.state.animal }).then(data => {
+        if (data.petfinder.breeds.breed) {
+          this.setState({ breeds: data.petfinder.breeds.breed });
+        } else {
+          this.setState({ breeds: [] });
+        }
+      });
+    } else {
+      this.setState({ breeds: [] });
+    }
+  }
   render() {
     return (
       <div>
@@ -13,11 +66,13 @@ class App extends React.Component {
           <Link to="/">Adopt me!</Link>
         </header>
 
-        <Router>
-          <Results path="/" />
-          <Details path="/details/:id" />
-          <SearchParams path="/search-params" />
-        </Router>
+        <Provider value={this.state}>
+          <Router>
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
+          </Router>
+        </Provider>
       </div>
     );
   }
